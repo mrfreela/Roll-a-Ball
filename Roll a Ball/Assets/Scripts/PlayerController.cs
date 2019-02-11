709a7af3ec7500e7a20a684686fdf7370a41aa1f
mrfreela <mrfreela@iu.edu>
2019-02-11 12:10:30 -0500

    added a script to the camera to make it follow the player, fixed player script slightly

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -19,7 +19,6 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (movement * speed
-	);
+        rb.AddForce (movement * speed);
     }
 }
