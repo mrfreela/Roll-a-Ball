efe0653075f9b4cc84ec2995e7055449693f3c6e
mrfreela <mrfreela@iu.edu>
2019-02-13 12:23:48 -0500

    added a counter for collectables, win text in process

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+	public Text countText;
+	public Text winText;
 
-    private Rigidbody rb;
+	private Rigidbody rb;
+	private int count;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
     }
 
     void FixedUpdate ()
@@ -27,6 +33,18 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("PickUp"))
 		{
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+			
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "You Win!";
 		}
 	}
 }
